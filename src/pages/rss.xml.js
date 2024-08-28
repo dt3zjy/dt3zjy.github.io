b5f93cc18820f06417810bf9ccea10690daa1da6
dt3zjy <dt3zjy@virginia.edu>
2024-08-28 17:29:21 -0400

    Fixed an error regarding an old blog object

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,16 +3,31 @@ import { SITE_TITLE, SITE_DESCRIPTION } from "../config";
 import { getCollection } from "astro:content";
 
 export async function get(context) {
-  const blog = await getCollection("blog");
+  const aerospaceProjects = await getCollection("aerospace-projects");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: import.meta.env.SITE,
-    items: blog.map((post) => ({
+    items: aerospaceProjects.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
-      link: `/blog/${post.slug}/`,
+      link: `/aerospace-projects/${post.slug}/`,
+    })),
+  });
+}
+
+export async function get(context) {
+  const csProjects = await getCollection("cs-projects");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: import.meta.env.SITE,
+    items: csProjects.map((post) => ({
+      title: post.data.title,
+      pubDate: post.data.pubDate,
+      description: post.data.description,
+      link: `/cs-projects/${post.slug}/`,
     })),
   });
 }
